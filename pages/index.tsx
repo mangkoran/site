0a235564c1bfe9b673b64c03b38c0fd8a1392dc8
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-08-17 22:19:59 +0800

    Add, implement tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import LayoutHome, { sitleTitle } from '../components/layout/home'
+// import 'tailwindcss/tailwind.css'
 
 const Home: NextPage = () => {
     return (
@@ -10,28 +11,45 @@ const Home: NextPage = () => {
             <Head>
                 <title>{sitleTitle}</title>
             </Head>
-            <div className={styles.container}>
+            <div className={styles.containerLight}>
                 <div className={styles.description}>
                     <h2>Halo, nama gua mangkoran!</h2>
                     <p>Selamat datang di website gua!</p>
                     <p>
-                        Website ini dibuat menggunakan <code className={styles.code} > yarn create next-app --ts</code>
+                        Website ini dibuat menggunakan{' '}
+                        <code className={styles.code}> yarn create next-app --ts</code>
                     </p>
                 </div>
             </div>
-            <div className={styles.container}>
+            <div className={styles.containerLight}>
                 <div className={styles.description}>
                     <h2>Sekilas tentang mangkoran</h2>
-                    <p>Gua adalah seorang pelajar yang sekarang sedang menempuh pendidikan{' '}
-                        <em>bachelor degree</em> di Universiti Teknologi Malaysia, Johor.
-                    </p>
-                    <p>Beberapa bahasa yang telah gua kuasai: C/C++, Python, Oracle DBMS, Java,{' '}
-                    HTML, CSS, PHP, JavaScript, TypeScript</p>
-
-                    <p>Beberapa <em>framework</em> yang telah gua kuasai: Jekyll, React.js, Next.js</p>
-
-                    <p>Beberapa <em>misc</em> yang telah gua kuasai: Apache, Docker, Linux, Git,{' '}
-                    GitHub</p>
+                    <ul>
+                        <li>
+                            <p>
+                                Seorang pelajar yang sekarang sedang menempuh pendidikan{' '}
+                                <em>bachelor degree</em> di Universiti Teknologi Malaysia, Johor.
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Beberapa bahasa yang telah dikuasai: C/C++, Python, Oracle DBMS, Java,{' '}
+                                HTML, CSS, PHP, JavaScript, TypeScript
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Beberapa <em>framework</em> yang telah dikuasai: Jekyll, React.js, Next.js,{' '}
+                                Bootstrap, TailwindCSS
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                Beberapa <em>misc</em> yang telah dikuasai: Apache, Docker, Linux, Git,{' '}
+                                GitHub
+                            </p>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </LayoutHome>
