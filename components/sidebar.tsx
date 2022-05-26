e21733396d65a4e3f5b0fd7080a44691cbab5666
Afiq Nazrie <afnazrie@gmail.com>
2022-05-26 22:03:19 +0800

    fix: add social account link

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -13,9 +13,9 @@ const Sidebar = ({ home }: { home?: boolean }) => {
       </ul >
       <ul>
         <li>GitHub: <Link href={`https://github.com/mangkoran`}><a target="_blank">@mangkoran</a></Link></li>
-        <li>LinkedIn: <Link href={`/`}><a target="_blank">Afiq Nazrie</a></Link></li>
-        <li>Discord: <Link href={`/`}><a target="_blank">mangkoran#4645</a></Link></li>
-        <li>Email: <Link href={`/`}><a target="_blank">afnazrie@gmail.com</a></Link></li>
+        <li>LinkedIn: <Link href={`https://www.linkedin.com/in/mangkoran/`}><a target="_blank">Afiq Nazrie</a></Link></li>
+        <li>Discord: <Link href={`https://discordapp.com/users/391547930550599680`}><a target="_blank">mangkoran#4645</a></Link></li>
+        <li>Email: <Link href={`mailto:afnazrie@gmail.com`}><a target="_blank">afnazrie@gmail.com</a></Link></li>
       </ul>
     </nav >
   )
