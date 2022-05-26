56c925075ebcddbfc51fe9587f59e38caddccd04
Afiq Nazrie <afnazrie@gmail.com>
2022-05-26 22:03:51 +0800

    feat: initial posts fetching

---
+++ lib/posts.tsx
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory: string = path.join(process.cwd(), 'posts');
+
+const getSortedPostsData = () => {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, '');
+
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents);
+
+    return {
+      id,
+      ...(matterResult.data as { date: string, title: string }),
+    };
+  });
+
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+const getAllPostIds = () => {
+  const fileNames = fs.readdirSync(postsDirectory)
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  });
+}
+
+export { getSortedPostsData };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,50 @@ import type { NextPage } from 'next'
 import clsx from 'clsx';
 import Head from 'next/head'
 import Layout from '../components/layout';
+import { getSortedPostsData } from '../lib/posts'
 
-const Home: NextPage = () => {
+const Home = ({ allPostsData }: {
+  allPostsData: {
+    date: string,
+    title: string,
+    id: string
+  }[]
+}) => {
   return (
     <Layout home>
       <Head>
         <title>Home</title>
       </Head>
       <h1 className={clsx("pb-10")}>Afiq Nazrie</h1>
-      <p>
+      <p className={clsx("pb-10")}>
         Computer Network and Security undergradute student at Universiti Teknologi Malaysia.
         Interested in cloud computing, Kubernetes, and home server.
       </p>
+      {allPostsData.map(({ id, date, title }: {
+        id: string,
+        date: string,
+        title: string
+      }) => (
+        <li key={id}>
+          {title}
+          <br />
+          {id}
+          <br />
+          {date}
+        </li>
+      ))}
     </Layout>
   )
 }
 
-export default Home
+const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export { getStaticProps };
+export default Home;
